1849006dca2e39128e6db15005224c2faee4fbe1
Dominik Gruber <mail@dominikgruber.com>
2015-05-26 19:49:15 +0200

    Load movie showtimes

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var slack = require('slack-notify')(process.env.SLACK_HOOK_URL);
+var when = require('when');
 
+var Skip = require('./skip.js');
 var IMDb = require('./imdb.js');
 var RottenTomatoes = require('./rottentomatoes.js');
 var rt = new RottenTomatoes(process.env.RT_API_KEY);
@@ -14,70 +16,94 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post('/', function (req, res) {
   var q = req.body.text;
-  // Search Rotten Tomatoes
-  rt.search(q, function(rtData) {
-    if (!rtData || rtData.id === '770948116') {   // The movie with the ID 770948116 gets usually returned for wrong input
-      res.status(404).send('Movie not found');
-      return;
-    }
 
-    // Search IMDb
-    var imdbCallback = function(imdbData) {
-
-      // Build Slack response
-      var fields = [{
-        title: 'Rotten Tomatoes',
-        value: '<' + rtData.links.alternate + '|' + (rtData.ratings.critics_score >= 0 ? rtData.ratings.critics_score + '%' : 'No rating') + '>',
-        short: true
-      }, {
-        title: 'IMDb',
-        value: (imdbData ? '<http://www.imdb.com/title/tt' + imdbData.imdbId + '|' + (imdbData.rating ? imdbData.rating + ' (' + imdbData.votes + ' votes)' : 'No rating') + '>' : 'Not found'),
-        short: true
-      }];
-      if ((imdbData && imdbData.year) || rtData.year) {
-        fields.push({
-          title: 'Year',
-          value: (imdbData && imdbData.year ? imdbData.year : rtData.year),
-          short: true
-        });
-      }
-      if ((imdbData && imdbData.runtime) || rtData.runtime) {
-        fields.push({
-          title: 'Runtime',
-          value: (imdbData && imdbData.runtime ? imdbData.runtime : rtData.runtime + ' min'),
-          short: true
-        });
+  // Fetch movie data from Rotten Tomatoes and IMDb
+  var dataPromise = when.promise(function(resolve, reject) {
+    rt.search(q, function(rtData) {
+      if (!rtData || rtData.id === '770948116') {   // The movie with the ID 770948116 gets usually returned for wrong input
+        reject();
+        return;
       }
 
-      // Send to Slack
-      slack.send({
-        username: 'moviebot',
-        icon_emoji: ':movie_camera:',
-        channel: req.body.channel_id,
-        text: '_' + req.body.command + ' ' + req.body.text + '_',
-        attachments: [{
-          title: rtData.title,
-          // title_link: rtData.links.alternate,
-          color: '#FDEE00',
-          image_url: (rtData.posters && rtData.posters.original !== 'http://d3biamo577v4eu.cloudfront.net/static/images/redesign/poster_default_thumb.gif' ? rtData.posters.original : null),
-          fields: fields
-        }]
+      var imdbId = (rtData.alternate_ids ? rtData.alternate_ids.imdb : null);
+      if (!imdbId) {
+        resolve({ 'rt': rtData, 'imdb': null });
+        return;
+      }
+
+      IMDb.search(imdbId, function(imdbData) {
+        resolve({ 'rt': rtData, 'imdb': imdbData });
       });
+    });
+  });
 
-      res.send();
-    };
+  // Fetch showtimes from Skip
+  var skipPromise = Skip.search(q);
 
-    var imdbId = (rtData.alternate_ids ? rtData.alternate_ids.imdb : null);
-    if (imdbId) {
-      IMDb.search(imdbId, imdbCallback);
-    } else {
-      imdbCallback(null);
+  // Respond to Slack
+  when.all([dataPromise, skipPromise]).then(function(data) {
+    var rtData = data[0].rt;
+    var imdbData = data[0].imdb;
+    var showtimes = data[1];
+
+    // Build Slack response
+    var fields = [{
+      title: 'Rotten Tomatoes',
+      value: '<' + rtData.links.alternate + '|' + (rtData.ratings.critics_score >= 0 ? rtData.ratings.critics_score + '%' : 'No rating') + '>',
+      short: true
+    }, {
+      title: 'IMDb',
+      value: (imdbData ? '<http://www.imdb.com/title/tt' + imdbData.imdbId + '|' + (imdbData.rating ? imdbData.rating + ' (' + imdbData.votes + ' votes)' : 'No rating') + '>' : 'Not found'),
+      short: true
+    }, {
+      title: 'Year',
+      value: (imdbData && imdbData.year ? imdbData.year : (rtData.year ? rtData.year : 'Unknown')),
+      short: true
+    }, {
+      title: 'Runtime',
+      value: (imdbData && imdbData.runtime ? imdbData.runtime : (rtData.runtime ? rtData.runtime + ' min' : 'Unknown')),
+      short: true
+    }];
+
+    if (showtimes) {
+      showtimes.forEach(function(el) {
+        if (el.cinemas) {
+          var showtimesStr = '';
+          el.cinemas.forEach(function(el2) {
+            showtimesStr += '<' + el2.url + '|' + el2.cinema + '>: ' + el2.showtimes.join(', ') + '\n';
+          });
+          fields.push({
+            title: el.date,
+            value: showtimesStr.trim()
+          });
+        }
+      });
     }
+
+    // Send to Slack
+    slack.send({
+      username: 'moviebot',
+      icon_emoji: ':movie_camera:',
+      channel: req.body.channel_id,
+      text: '_' + req.body.command + ' ' + req.body.text + '_',
+      attachments: [{
+        title: rtData.title,
+        // title_link: rtData.links.alternate,
+        color: '#FDEE00',
+        image_url: (rtData.posters && rtData.posters.original !== 'http://d3biamo577v4eu.cloudfront.net/static/images/redesign/poster_default_thumb.gif' ? rtData.posters.original : null),
+        fields: fields
+      }]
+    });
+
+    res.send();
+  }).catch(function(e) {
+    console.log('[ERROR] ' + e);
+    res.status(404).send('Movie not found');
   });
 });
 
 var port = process.env.PORT || 3000;
-var server = app.listen(port, function () {
+var server = app.listen(port, function() {
   var host = server.address().address;
   var port = server.address().port;
   console.log('App listening at http://%s:%s', host, port);
---
+++ src/skip.js
@@ -0,0 +1,87 @@
+'use strict';
+
+var when = require('when');
+var request = require('request');
+var querystring = require('querystring');
+var cheerio = require('cheerio');
+
+var Skip = {};
+
+Skip.search = function(name) {
+  return when.promise(function(resolve, reject) {
+    request('http://www.skip.at/suche/?q=' + querystring.escape(name), function(error, response, body) {
+      if (error || response.statusCode !== 200) {
+        resolve(null);
+        return;
+      }
+
+      var skipId = getSkipMovieIdFromBody(body);
+      if (!skipId) {
+        resolve(null);
+        return;
+      }
+
+      // Fetch showtimes for the next 7 days
+      var showtimePromises = [];
+      var d = new Date();
+      for (var i = 0; i < 7; i++) {
+        showtimePromises.push(findShowtimes(skipId, d.toISOString().substr(0, 10)));
+        d.setDate(d.getDate() + 1);
+      }
+
+      when.all(showtimePromises).then(function(showtimes) {
+        resolve(showtimes);
+      });
+    });
+  });
+};
+
+function getSkipMovieIdFromBody(body) {
+  var $ = cheerio.load(body);
+  var link = $('.box.listing ul h2 a').first().attr('href');
+  var m = (link ? link.match(/\/film\/(\d+)\/?/) : null);
+  return (m && m.length >= 2 ? m[1] : null);
+}
+
+function findShowtimes(skipId, date) {
+  return when.promise(function(resolve, reject) {
+    var url = 'http://www.skip.at/kinoprogramm/wien/filme/?filter=OF&film=' + skipId + '&datum=' + date;
+    request(url, function(error, response, body) {
+      var data = {
+        date: date,
+        cinemas: []
+      };
+
+      if (error || response.statusCode !== 200) {
+        resolve(data);
+        return;
+      }
+
+      var $ = cheerio.load(body);
+      $('ul.schedule-listing li').each(function(idx, el) {
+        var cinemaLink = $(this).find('h4 a').attr('href');
+        var m = (cinemaLink ? cinemaLink.match(/\/kino\/(\d+)\/?/) : null);
+        var cinemaId = (m && m.length >= 2 ? m[1] : null);
+        var entry = {
+          cinema: $(this).find('h4 a').text(),
+          url: 'http://www.skip.at/kinoprogramm/wien/kinos/?filter=OF&kino=' + cinemaId + '&datum=' + date,
+          showtimes: []
+        };
+        $(this).find('div.times div.time').each(function(idx2, el2) {
+          var meta = [];
+          $(this).find('span.additional').each(function(idx3, el3) {
+            meta.push($(this).text());
+            $(this).text('');
+          });
+          var showtime = $(this).text().trim().replace('.', ':') + (meta.length ? ' (' + meta.join(' ') + ')' : '');
+          entry.showtimes.push(showtime);
+        });
+        data.cinemas.push(entry);
+      });
+
+      resolve(data);
+    });
+  });
+}
+
+module.exports = Skip;
